deada1b17db76886ca9801f12c12843242747ad2
IagoSantos <iago.olra.santos@gmail.com>
2022-03-20 19:34:18 -0300

    chore: configuracoes tsconfig e vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import alias from '@rollup/plugin-alias'
+
+const rootDir = resolve(__dirname)
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+    plugins: [vue(), alias()],
+    resolve: {
+        alias: {
+            '@': resolve(rootDir, './src'),
+            '@/tests': resolve(rootDir, './test/*')
+        }
+    }
 })
